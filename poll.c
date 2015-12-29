2d9d587f18e7006689968c2a1c5f412b241da24a
Pascal Mainini <pascal@mainini.ch>
2015-12-29 02:53:45 +0100

    cleanup, added license

--- poll.c
+++ poll.c
@@ -1,12 +1,18 @@
+/*
+ * Copyright 2015 Pascal Mainini
+ * Licensed under MIT license, see included file LICENSE or
+ * http://opensource.org/licenses/MIT
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #define NXP_HW_SELF_TEST
 #include "linux_nfc_factory_api.h"
 #include "linux_nfc_api.h"
 
-static nfcTagCallback_t cb_tag;
-static int keepPolling = 1;
+#define MAIN_SLEEP 10000
 
 void tagArrived(nfc_tag_info_t *p_taginfo)
 {
@@ -16,7 +22,6 @@ void tagArrived(nfc_tag_info_t *p_taginfo)
 void tagDeparted(void)
 {
     printf("\nTag departed!\n");
-    keepPolling = 0;
 }
 
 int main(int argc, char ** argv)
@@ -25,26 +30,27 @@ int main(int argc, char ** argv)
     int result = nfcManager_doInitialize();
     if(result != 0) {
         printf("nfcManager_doInitialize failed with error code %d", result);
-        return result;
+        exit(result);
     }
 
     // try to get versions
-#ifdef NXP_HW_SELF_TEST
-    printf("nfcFactory_GetMwVersion:\t%x\n", nfcFactory_GetMwVersion());
-#endif
-    printf("nfcManager_getFwVersion:\t%x\n", nfcManager_getFwVersion());
+    #ifdef NXP_HW_SELF_TEST
+    printf("nfcFactory_GetMwVersion:\t0x%x\n", nfcFactory_GetMwVersion());
+    #endif
+    printf("nfcManager_getFwVersion:\t0x%x\n\n", nfcManager_getFwVersion());
 
     // set up tag-callback
+    nfcTagCallback_t cb_tag;
 	cb_tag.onTagArrival = tagArrived;
 	cb_tag.onTagDeparture = tagDeparted;
 	nfcManager_registerTagCallback(&cb_tag);
 
 	nfcManager_enableDiscovery(DEFAULT_NFA_TECH_MASK, 0x00, 0x00, 0);
 
-    while(keepPolling) {
-        usleep(10000);
+    while(1) {
+        usleep(MAIN_SLEEP);
     }
 
-    printf("\nExiting...\n");
+    printf("\n\nExiting...\n");
     return 0;
 }
